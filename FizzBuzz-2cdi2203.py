364a3cc268cbc06f764ce4ae36d52bae6fc93bb1
Takaya Miura <2cdi2203@tokai.ac.jp>
2024-10-04 14:25:03 +0900

    test

---
+++ FizzBuzz-2cdi2203.py
@@ -0,0 +1,20 @@
+x = 0
+y = int(input("整数を入力してください。"))
+
+for x in range(y):
+    x += 1
+
+    if (x % 3 == 0) and (x % 5 == 0):
+        print("Fizz Buzz")
+        continue
+        
+    elif x % 5 == 0:
+        print("Buzz")
+        continue
+
+    elif x % 3 == 0:
+        print("Fizz ")
+        continue
+
+    else:
+        print(x)  
